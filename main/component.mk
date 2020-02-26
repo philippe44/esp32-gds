c0c6143a61f325e2e240d35023d73fdaa1b30d7a
philippe44 <philippe_44@outlook.com>
2020-02-26 00:49:04 -0800

    cleanup

--- main/component.mk
+++ main/component.mk
@@ -1,4 +1,4 @@
 #
 # Main Makefile. This is basically the same as a component makefile.
 #
-COMPONENT_EMBED_FILES := image.jpg image2.jpg  image_stella.jpg
+COMPONENT_EMBED_FILES := image.jpg image2.jpg  image3.jpg
--- main/decode_image.c
+++ main/decode_image.c
@@ -25,8 +25,8 @@ format if you want to use a different image file.
 #include <string.h>
 
 //Reference the binary-included jpeg file
-extern const uint8_t image_jpg_start[]   asm("_binary_image_stella_jpg_start");
-extern const uint8_t image_jpg_end[]     asm("_binary_image_stella_jpg_end");
+extern const uint8_t image_jpg_start[]   asm("_binary_image3_jpg_start");
+extern const uint8_t image_jpg_end[]     asm("_binary_image3_jpg_end");
 //Define the height and width of the jpeg file. Make sure this matches the actual jpeg
 //dimensions.
 #define IMAGE_W 96
